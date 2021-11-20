60aa193a81077ff712ab13efc90cafb0d4aba30e
PRIYESH <priyeshlenka50@gmail.com>
2021-11-20 19:53:36 +0530

    chooser

--- app/src/main/java/com/nextgen/wego/GenerateOTP.java
+++ app/src/main/java/com/nextgen/wego/GenerateOTP.java
@@ -3,7 +3,10 @@ package com.nextgen.wego;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GenerateOTP extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class GenerateOTP extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_otp);
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        getSupportActionBar().hide();
+        Button generateOTP = (Button)findViewById(R.id.GenerateOTP);
+
+        generateOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(GenerateOTP.this, EnterOtp.class));
+            }
+        });
+        finish();
     }
 }
