c2f953f9b5bb3b2da1ceb54afdc60a18ab7c5b97
GOURAV kumar <gouravkumar05112000@gmail.com>
2021-11-26 20:01:19 +0530

    customer detail

--- app/src/main/java/com/nextgen/wego/CustomerDetail.java
+++ app/src/main/java/com/nextgen/wego/CustomerDetail.java
@@ -10,6 +10,6 @@ public class CustomerDetail extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_detail);
-        getSupportActionBar().hide();
+
     }
 }
